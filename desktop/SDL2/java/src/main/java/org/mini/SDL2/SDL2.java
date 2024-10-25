c362245c2da8c5b4cba448c7d076a4878b231a19
unknown <kindred77>
2024-10-25 11:02:34 +0800

    modify

--- desktop/SDL2/java/src/main/java/org/mini/SDL2/SDL2.java
+++ desktop/SDL2/java/src/main/java/org/mini/SDL2/SDL2.java
@@ -111,8 +111,9 @@ public class SDL2 {
 
             MirLib mir_lib =new MirLib("C:/mywork/projects/cpp/devilutionX/kindred/devilutionX/my_asset/Prguse2_png.Lib");
             mir_lib.Initialize();
-            System.out.println("--44--image count: "+mir_lib.GetImageCount()+", img.data.length: "+img.data.length+", img.header.length: "+img.header.length);
+            
             MirImage img = mir_lib.GetMirImage(542);
+            System.out.println("--44--image count: "+mir_lib.GetImageCount()+", img.data.length: "+img.data.length+", img.header.length: "+img.header.length);
 
             long rwops_id = SDL_RWFromConstMem(img.data, img.header.length);
             if (rwops_id == 0) {
