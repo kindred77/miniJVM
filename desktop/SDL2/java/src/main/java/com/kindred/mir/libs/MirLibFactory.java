6fe5cfb1cc9226f42bd7a5ab46bfb51e3fb1e9d0
kindred77 <109327586@qq.com>
2025-05-28 17:44:39 +0800

    modify

--- desktop/SDL2/java/src/main/java/com/kindred/mir/libs/MirLibFactory.java
+++ desktop/SDL2/java/src/main/java/com/kindred/mir/libs/MirLibFactory.java
@@ -97,50 +97,6 @@ public class MirLibFactory implements Runnable{
 
     public static void StartToInit() {
 
-        //开启后台线程异步加载其它素材
-        Thread thread = new Thread(new MirLibFactory());
-        thread.start();
-
-        try {
-            //基础素材,如果加载不成功就不能启动
-            loadBaseLibraries();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void run() {
-        loadGameLibraries();
-        System.out.println("All libs initialized.");
-    }
-
-    private static void loadBaseLibraries()
-    {
-        if(!ChrSel.Initialize(false)) {
-            throw new RuntimeException("ChrSel initialize failed.");
-        }
-        Progress++;
-
-        if(!Prguse.Initialize(false)) {
-            throw new RuntimeException("Prguse initialize failed.");
-        }
-        Progress++;
-
-        //Prguse2.Initialize();
-        //Progress++;
-
-        if(!Prguse3.Initialize(false)) {
-            throw new RuntimeException("Prguse3 initialize failed.");
-        }
-        Progress++;
-
-        //Title.Initialize();
-        //Progress++;
-    }
-
-    private static void loadGameLibraries()
-    {
         //Wiz/War/Tao
         for (int i = 0; i < CArmours.length; i++)
             CArmours[i] = new MirLib(Settings.CArmourPath + String.format("%02d", i));
@@ -220,64 +176,109 @@ public class MirLibFactory implements Runnable{
 
         //map libs
         //wemade mir2 (allowed from 0-99)
-        mapLibs[0] = new MirLib(Settings.DataPath+"WemadeMir2\\Tiles");
-        mapLibs[1] = new MirLib(Settings.DataPath+"WemadeMir2\\Smtiles");
-        mapLibs[2] = new MirLib(Settings.DataPath+"WemadeMir2\\Objects");
+        mapLibs[0] = new MirLib(Settings.DataPath+"Map\\WemadeMir2\\Tiles");
+        mapLibs[1] = new MirLib(Settings.DataPath+"Map\\WemadeMir2\\Smtiles");
+        mapLibs[2] = new MirLib(Settings.DataPath+"Map\\WemadeMir2\\Objects");
         for (int i = 2; i < 24; i++) {
-            mapLibs[i + 1] = new MirLib(Settings.DataPath+"WemadeMir2\\Objects" + i);
+            mapLibs[i + 1] = new MirLib(Settings.DataPath+"Map\\WemadeMir2\\Objects" + i);
         }
         //shanda mir2 (allowed from 100-199)
-        mapLibs[100] = new MirLib(Settings.DataPath+"ShandaMir2\\Tiles");
+        mapLibs[100] = new MirLib(Settings.DataPath+"Map\\ShandaMir2\\Tiles");
         for (int i = 1; i < 10; i++) {
-            mapLibs[100 + i] = new MirLib(Settings.DataPath+"ShandaMir2\\Tiles" + (i + 1));
+            mapLibs[100 + i] = new MirLib(Settings.DataPath+"Map\\ShandaMir2\\Tiles" + (i + 1));
         }
-        mapLibs[110] = new MirLib(Settings.DataPath+"ShandaMir2\\SmTiles");
+        mapLibs[110] = new MirLib(Settings.DataPath+"Map\\ShandaMir2\\SmTiles");
         for (int i = 1; i < 10; i++)
         {
-            mapLibs[110 + i] = new MirLib(Settings.DataPath+"ShandaMir2\\SmTiles" + (i + 1));
+            mapLibs[110 + i] = new MirLib(Settings.DataPath+"Map\\ShandaMir2\\SmTiles" + (i + 1));
         }
-        mapLibs[120] = new MirLib(Settings.DataPath+"ShandaMir2\\Objects");
+        mapLibs[120] = new MirLib(Settings.DataPath+"Map\\ShandaMir2\\Objects");
         for (int i = 1; i < 31; i++) {
-            mapLibs[120 + i] = new MirLib(Settings.DataPath+"ShandaMir2\\Objects" + (i + 1));
+            mapLibs[120 + i] = new MirLib(Settings.DataPath+"Map\\ShandaMir2\\Objects" + (i + 1));
         }
-        mapLibs[190] = new MirLib(Settings.DataPath+"ShandaMir2\\AniTiles1");
+        mapLibs[190] = new MirLib(Settings.DataPath+"Map\\ShandaMir2\\AniTiles1");
         //wemade mir3 (allowed from 200-299)
         String[] mapState = { "", "wood\\", "sand\\", "snow\\", "forest\\"};
         for (int i = 0; i < mapState.length; i++) {
-            mapLibs[200 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Tilesc");
-            mapLibs[201 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Tiles30c");
-            mapLibs[202 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Tiles5c");
-            mapLibs[203 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Smtilesc");
-            mapLibs[204 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Housesc");
-            mapLibs[205 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Cliffsc");
-            mapLibs[206 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Dungeonsc");
-            mapLibs[207 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Innersc");
-            mapLibs[208 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Furnituresc");
-            mapLibs[209 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Wallsc");
-            mapLibs[210 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "smObjectsc");
-            mapLibs[211 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Animationsc");
-            mapLibs[212 +(i*15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Object1c");
-            mapLibs[213 + (i * 15)] = new MirLib(Settings.DataPath+"WemadeMir3\\" + mapState[i] + "Object2c");
+            mapLibs[200 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Tilesc");
+            mapLibs[201 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Tiles30c");
+            mapLibs[202 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Tiles5c");
+            mapLibs[203 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Smtilesc");
+            mapLibs[204 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Housesc");
+            mapLibs[205 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Cliffsc");
+            mapLibs[206 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Dungeonsc");
+            mapLibs[207 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Innersc");
+            mapLibs[208 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Furnituresc");
+            mapLibs[209 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Wallsc");
+            mapLibs[210 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "smObjectsc");
+            mapLibs[211 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Animationsc");
+            mapLibs[212 +(i*15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Object1c");
+            mapLibs[213 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\WemadeMir3\\" + mapState[i] + "Object2c");
         }
         mapState = new String[] { "", "wood", "sand", "snow", "forest"};
         //shanda mir3 (allowed from 300-399)
         for (int i = 0; i < mapState.length; i++) {
-            mapLibs[300 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Tilesc" + mapState[i]);
-            mapLibs[301 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Tiles30c" + mapState[i]);
-            mapLibs[302 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Tiles5c" + mapState[i]);
-            mapLibs[303 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Smtilesc" + mapState[i]);
-            mapLibs[304 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Housesc" + mapState[i]);
-            mapLibs[305 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Cliffsc" + mapState[i]);
-            mapLibs[306 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Dungeonsc" + mapState[i]);
-            mapLibs[307 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Innersc" + mapState[i]);
-            mapLibs[308 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Furnituresc" + mapState[i]);
-            mapLibs[309 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Wallsc" + mapState[i]);
-            mapLibs[310 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "smObjectsc" + mapState[i]);
-            mapLibs[311 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Animationsc" + mapState[i]);
-            mapLibs[312 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Object1c" + mapState[i]);
-            mapLibs[313 + (i * 15)] = new MirLib(Settings.DataPath+"ShandaMir3\\" + "Object2c" + mapState[i]);
+            mapLibs[300 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Tilesc" + mapState[i]);
+            mapLibs[301 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Tiles30c" + mapState[i]);
+            mapLibs[302 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Tiles5c" + mapState[i]);
+            mapLibs[303 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Smtilesc" + mapState[i]);
+            mapLibs[304 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Housesc" + mapState[i]);
+            mapLibs[305 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Cliffsc" + mapState[i]);
+            mapLibs[306 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Dungeonsc" + mapState[i]);
+            mapLibs[307 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Innersc" + mapState[i]);
+            mapLibs[308 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Furnituresc" + mapState[i]);
+            mapLibs[309 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Wallsc" + mapState[i]);
+            mapLibs[310 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "smObjectsc" + mapState[i]);
+            mapLibs[311 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Animationsc" + mapState[i]);
+            mapLibs[312 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Object1c" + mapState[i]);
+            mapLibs[313 + (i * 15)] = new MirLib(Settings.DataPath+"Map\\ShandaMir3\\" + "Object2c" + mapState[i]);
         }
 
+        //开启后台线程异步加载其它素材
+        Thread thread = new Thread(new MirLibFactory());
+        thread.start();
+
+        try {
+            //基础素材,如果加载不成功就不能启动
+            loadBaseLibraries();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void run() {
+        //TODO 异步加载素材会有冲突，而且启用异步加载素材似乎也什么用，暂时不用启用
+        //loadGameLibraries();
+        //System.out.println("All libs initialized.");
+    }
+
+    private static void loadBaseLibraries()
+    {
+        if(!ChrSel.Initialize(false)) {
+            throw new RuntimeException("ChrSel initialize failed.");
+        }
+        Progress++;
+
+        if(!Prguse.Initialize(false)) {
+            throw new RuntimeException("Prguse initialize failed.");
+        }
+        Progress++;
+
+        //Prguse2.Initialize();
+        //Progress++;
+
+        if(!Prguse3.Initialize(false)) {
+            throw new RuntimeException("Prguse3 initialize failed.");
+        }
+        Progress++;
+
+        //Title.Initialize();
+        //Progress++;
+    }
+
+    private static void loadGameLibraries()
+    {
         Dragon.Initialize(true);
         Progress++;
 
